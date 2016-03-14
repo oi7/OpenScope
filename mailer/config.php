ab6c034e691cd39d3598bb54abb2f5bcd47157d7
oi7 <oi7@mit.edu>
2016-03-14 02:55:24 -0400

    first commit

---
+++ mailer/config.php
@@ -0,0 +1,15 @@
+<?php
+
+$my_email = 'oi7@mit.edu';
+
+$SMTP = array(
+    'enabled' => true,
+    'host' => 'smtp.gmail.com',
+    'username' => 'oi7@mit.edu',
+    'password' => 'password12345',
+    'port' => 465,
+    'encryption' => 'ssl'
+);
+
+// Available colors: blue, green, grey, red and yellow
+$template_color = 'grey';
